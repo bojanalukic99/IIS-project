0ac5f5e4f597b8a6f15ac60ff0c48b1e570fb53a
Bojana Lukic <lukicb99@gmail.com>
2022-09-10 17:41:14 +0200

    design update

--- Frontend/src/app/home-page/home-page.component.ts
+++ Frontend/src/app/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 @Component({
   selector: 'app-home-page',
@@ -9,23 +9,57 @@ import { ApiService } from '../api.service';
 })
 export class HomePageComponent implements OnInit {
 
-
-  constructor(private formBuilder: FormBuilder, private api : ApiService, private activatedRoute: ActivatedRoute, private router: Router) 
+  form: FormGroup;
+  user: any;
+  constructor(private formBuilder: FormBuilder, private api : ApiService, private router: Router) 
   {
-     
+    this.form = this.formBuilder.group({
+      email: ['', Validators.email],
+      password: ['', Validators.required]
+    });
   }
   ngOnInit(): void {
   }
 
+  onSubmit() {
 
+    this.api.login({
+      email: this.form.get('email')?.value,
+      password: this.form.get('password')?.value,
+      clientId: 'ClientID',
+      clientSecret: 'ClientSecret'
+    }).subscribe((response: any) => {
+      console.log(response);
+      localStorage.setItem('token', response.token)
+
+      this.api.getCurrentUser().subscribe((response2: any) => {
+
+        console.log(response2);
+
+       localStorage.setItem('user', JSON.stringify(response2))
+
+       this.user = this.api.getUserFromLocalstorage();
+
+
+        if(this.user.userType == 1){
+          this.router.navigate(['/optician-home-page']);
+        }
+        else {
+          this.router.navigate(['/nurse-home-page']);
+        }
+
+        
+    }); 
+      
+
+    });
+  
+  }
   navigate(data : any){
-    if(data === 'login'){
+    if(data === 'contact'){
       this.router.navigate(['/login']);
     }
 
-    else if(data === 'registration'){
-      this.router.navigate(['/registration']);
-    }
     else if(data === 'home'){
       this.router.navigate(['/home-page']);
     }
